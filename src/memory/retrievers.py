6179387ef7e9d3250d26c9d67d83334a2ef8c6b0
Serge Mosin <svmosin@gmail.com>
2023-11-17 13:20:33 +0000

    Create a Retriever wrapper for LlamaIndex

---
+++ src/memory/retrievers.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+
+from langchain.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain.pydantic_v1 import Field
+from langchain.schema import BaseRetriever, Document
+
+from src.vdb import retrieve
+
+
+class LlamaVectorIndexRetriever(BaseRetriever):
+    """`LlamaIndex` retriever.
+
+    It is used for the question-answering with sources over
+    an LlamaIndex data structure."""
+
+    query_kwargs: Dict = Field(default_factory=dict)
+    """Keyword arguments to pass to the query method."""
+
+    def _get_relevant_documents(
+            self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Get documents relevant for a query."""
+        nodes = retrieve(query, **self.query_kwargs)
+
+        return [Document(page_content=node.text, metadata=node.metadata)
+                for node in nodes]
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
