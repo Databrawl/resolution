48897174c9387e175e8861f250f7706a3947e61f
Serge Mosin <svmosin@gmail.com>
2023-11-20 13:56:48 +0000

    Add Alembic configuration and initial migrations

---
+++ src/db/models.py
@@ -0,0 +1,50 @@
+import re
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.ext.declarative import DeferredReflection
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    """subclasses will be converted to dataclasses"""
+    __abstract__ = True
+
+
+class BaseId(Base):
+    """subclasses will be converted to dataclasses"""
+    __abstract__ = True
+
+    # noinspection PyMethodParameters
+    @declared_attr
+    def __tablename__(cls):
+        """Convert class name to snake_case table name"""
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', cls.__name__).lower()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class Reflected(DeferredReflection):
+    __abstract__ = True
+
+
+class User(Base, Reflected):
+    __tablename__ = 'users'
+    __table_args__ = {'schema': 'auth'}
+
+
+class Org(BaseId):
+    name: Mapped[str] = mapped_column(String(30))
+
+
+class OrgUser(BaseId):
+    # Many-to-many between users and orgs, we need since we cannot modify Users table that is set by Supabase
+    user_id = mapped_column(ForeignKey('auth.users.id'))
+    org_id: Mapped[int] = mapped_column(ForeignKey(Org.id))
+
+# class Chunk(Base):
+#     # id: Mapped[int] = mapped_column(primary_key=True)
+#     org_id: Mapped[int] = mapped_column(ForeignKey(Org.id))
+#
+#     embedding = mapped_column(Vector(DEFAULT_EMBEDDING_DIM))
