954ae05c5ff6d45aebb8a1ac29310223c4e14267
Serge Mosin <svmosin@gmail.com>
2023-11-22 12:57:19 +0000

    Recreate migrations with Chunks

--- src/db/models.py
+++ src/db/models.py
@@ -1,8 +1,10 @@
 import re
+from uuid import uuid4
 
 from llama_index.constants import DEFAULT_EMBEDDING_DIM
 from pgvector.sqlalchemy import Vector
 from sqlalchemy import String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import DeferredReflection
 from sqlalchemy.orm import DeclarativeBase, declared_attr, relationship
 from sqlalchemy.orm import Mapped
@@ -13,18 +15,13 @@ class Base(DeclarativeBase):
     """subclasses will be converted to dataclasses"""
     __abstract__ = True
 
-
-class BaseId(Base):
-    """subclasses will be converted to dataclasses"""
-    __abstract__ = True
-
     # noinspection PyMethodParameters
     @declared_attr
     def __tablename__(cls):
         """Convert class name to snake_case table name"""
         return re.sub('([a-z0-9])([A-Z])', r'\1_\2', cls.__name__).lower()
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
 
 
 class Reflected(DeferredReflection):
@@ -36,20 +33,19 @@ class User(Base, Reflected):
     __table_args__ = {'schema': 'auth'}
 
 
-class Org(BaseId):
+class Org(Base):
     name: Mapped[str] = mapped_column(String(30))
-
     # relationships
     chunks = relationship("Chunk", backref="org")
 
 
-class OrgUser(BaseId):
+class OrgUser(Base):
     # Many-to-many between users and orgs, we need since we cannot modify Users table that is set by Supabase
     user_id = mapped_column(ForeignKey('auth.users.id'))
     org_id: Mapped[int] = mapped_column(ForeignKey(Org.id))
 
 
-class Chunk(BaseId):
+class Chunk(Base):
     org_id: Mapped[int] = mapped_column(ForeignKey(Org.id))
 
     embedding = mapped_column(Vector(DEFAULT_EMBEDDING_DIM))
