2bc3c87b30b3cef43127ba596cf628698e9c251c
Serge Mosin <svmosin@gmail.com>
2023-11-27 12:39:50 +0000

    Add generic test config

---
+++ src/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from config import settings
+from db.models import Reflected
+
+
+@pytest.fixture(scope="session")
+def engine():
+    # TODO: seed test db with auth schema data, then switch to test db
+    return create_engine(settings.SUPABASE_DB)
+
+
+@pytest.fixture
+def dbsession(engine):
+    """Returns an sqlalchemy session, and after the test tears down everything properly."""
+    Reflected.prepare(engine)
+
+    connection = engine.connect()
+    # begin the nested transaction
+    transaction = connection.begin()
+    # use the connection with the already started transaction
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    # roll back the broader transaction
+    transaction.rollback()
+    # put back the connection to the connection pool
+    connection.close()
--- src/db/tests.py
+++ src/db/tests.py
@@ -2,37 +2,9 @@ from uuid import uuid4
 
 import pytest
 from llama_index.constants import DEFAULT_EMBEDDING_DIM
-from sqlalchemy import create_engine, exc, select
-from sqlalchemy.orm import Session
+from sqlalchemy import exc, select
 
-from config import settings
-from db.models import Chunk, Reflected, Org, User, OrgUser
-
-
-@pytest.fixture(scope="session")
-def engine():
-    # TODO: seed test db with auth schema data, then switch to test db
-    return create_engine(settings.SUPABASE_DB)
-
-
-@pytest.fixture
-def dbsession(engine):
-    """Returns an sqlalchemy session, and after the test tears down everything properly."""
-    Reflected.prepare(engine)
-
-    connection = engine.connect()
-    # begin the nested transaction
-    transaction = connection.begin()
-    # use the connection with the already started transaction
-    session = Session(bind=connection)
-
-    yield session
-
-    session.close()
-    # roll back the broader transaction
-    transaction.rollback()
-    # put back the connection to the connection pool
-    connection.close()
+from db.models import Chunk, Org, User, OrgUser
 
 
 class TestBase:
