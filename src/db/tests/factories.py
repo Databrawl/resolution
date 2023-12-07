953af992dc90474d05db55df100aa8c52bbe09d3
Serge Mosin <svmosin@gmail.com>
2023-12-07 10:09:30 +0100

    Add new factories

--- src/db/tests/factories.py
+++ src/db/tests/factories.py
@@ -1,7 +1,12 @@
+import random
+from hashlib import sha256
+
 import factory
+from factory import lazy_attribute
+from llama_index.constants import DEFAULT_EMBEDDING_DIM
 
 from db import db_session
-from db.models import User, Org
+from db.models import User, Org, Chunk, OrgUser
 
 
 class Options(factory.alchemy.SQLAlchemyOptions):
@@ -39,3 +44,36 @@ class OrgFactory(ModelFactory):
         model = Org
 
     name = factory.Faker("company")
+
+
+class OrgUserFactory(ModelFactory):
+    class Meta:
+        model = OrgUser
+
+    org = factory.SubFactory(OrgFactory)
+    user = factory.SubFactory(UserFactory)
+
+
+class ChunkFactory(ModelFactory):
+    class Meta:
+        model = Chunk
+
+    org = factory.SubFactory(OrgFactory)
+    data = factory.Dict(
+        {
+            "text": factory.Faker("text"),
+            "metadata": factory.Dict(
+                {
+                    "key": factory.Faker("name")
+                }
+            )
+        }
+    )
+
+    @lazy_attribute
+    def hash_value(self):
+        return sha256(self.data["text"].encode()).hexdigest()
+
+    @lazy_attribute
+    def embedding(self):
+        return [round(random.random(), 2) for _ in range(DEFAULT_EMBEDDING_DIM)]
