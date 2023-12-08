93aee8e592173479202ae0d77c169bc5b764cee0
Serge Mosin <svmosin@gmail.com>
2023-12-08 16:27:01 +0100

    Add Org if not exists when initializing VDB

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ import sys
 from pprint import pprint
 
 from sqlalchemy import select
+from sqlalchemy.orm import exc
 
 from bots.chain_s2 import get_chain
 from bots.librarian import librarian_agent
 from config import settings
 from db.core import get_db, current_org, db_session
 from db.models import Org
+from db.tests.factories import OrgFactory
 from memory.functions import search_native_formatted
 from memory.utils import archive_urls, retrieve
 
@@ -32,16 +34,15 @@ def main():
     parser.add_argument('mode', type=str, help='Application operation mode. One of: "vdb", "librarian", "chat"')
     parser.add_argument('org', type=str, help='The Organization name')
     parser.add_argument('--query', type=str, help='String to query Vector Database for', default=None)
-    # Execute the parse_args() method
     args = parser.parse_args()
+
     # set the context vars
     session = next(get_db())
-    # db_session_ctx = contextvars.ContextVar("db_session")
-    # db_session_ctx.set(session)
     db_session.set(session)
-    org = session.execute(select(Org).where(Org.name == args.org)).scalar_one()
-    # cur_org_ctx = contextvars.ContextVar("current_org")
-    # cur_org_ctx.set(org)
+    try:
+        org = session.execute(select(Org).where(Org.name == args.org)).scalar_one()
+    except exc.NoResultFound:
+        org = OrgFactory.create(name=args.org)
     current_org.set(org)
 
     if args.mode == 'vdb':
