eb2bc42357d1af640ee94e9c0076911e2c61b570
Serge Mosin <svmosin@gmail.com>
2023-12-16 19:41:21 +0100

    Move base models to the models module

--- server/db/database.py
+++ server/db/database.py
@@ -1,24 +1,14 @@
 from __future__ import annotations
 
-import re
 from contextlib import contextmanager
 from contextvars import ContextVar
 from typing import Any
-from typing import Callable, ClassVar, Dict, Generator, Iterator, List, Optional, Set, cast
+from typing import Generator, Iterator, Optional
 from uuid import uuid4
 
 import structlog
-from sqlalchemy import ForeignKey
 from sqlalchemy import create_engine
-from sqlalchemy import inspect as sa_inspect
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.declarative import as_declarative, DeclarativeMeta, DeferredReflection
-from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import Query, Session, scoped_session, sessionmaker
-from sqlalchemy.orm import declared_attr
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm.state import InstanceState
-from sqlalchemy.sql.schema import MetaData
 from sqlalchemy_searchable import SearchQueryMixin
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
@@ -39,154 +29,6 @@ class SearchQuery(Query, SearchQueryMixin):
 
 class NoSessionError(RuntimeError):
     pass
-
-
-class BaseModelMeta(DeclarativeMeta):
-    """
-    Using this metaclass means that we can set and access query as a property at a class level.
-    """
-
-    def set_query(self, query: SearchQuery) -> None:
-        self._query = query
-
-    @property
-    def query(self) -> SearchQuery:
-        if self._query is not None:
-            return self._query
-        else:
-            raise NoSessionError("Can't get session. Please, call BaseModel.set_query() first")
-
-
-# @as_declarative(metaclass=BaseModelMeta)
-@as_declarative()
-class _Base:
-    """SQLAlchemy base class."""
-
-    __abstract__ = True
-
-    _json_include: List = []
-    _json_exclude: List = []
-
-    def __json__(self, excluded_keys: Set = set()) -> Dict:  # noqa: B006
-        ins = sa_inspect(self)
-
-        columns = set(ins.mapper.column_attrs.keys())
-        relationships = set(ins.mapper.relationships.keys())
-        unloaded = ins.unloaded
-        expired = ins.expired_attributes
-        include = set(self._json_include)
-        exclude = set(self._json_exclude) | set(excluded_keys)
-
-        # This set of keys determines which fields will be present in
-        # the resulting JSON object.
-        # Here we initialize it with properties defined by the model class,
-        # and then add/delete some columns below in a tricky way.
-        keys = columns | relationships
-
-        # 1. Remove not yet loaded properties.
-        # Basically this is needed to serialize only .join()'ed relationships
-        # and omit all other lazy-loaded things.
-        if not ins.transient:
-            # If the entity is not transient -- exclude unloaded keys
-            # Transient entities won't load these anyway, so it's safe to
-            # include all columns and get defaults
-            keys -= unloaded
-
-        # 2. Re-load expired attributes.
-        # At the previous step (1) we substracted unloaded keys, and usually
-        # that includes all expired keys. Actually we don't want to remove the
-        # expired keys, we want to refresh them, so here we have to re-add them
-        # back. And they will be refreshed later, upon first read.
-        if ins.expired:
-            keys |= expired
-
-        # 3. Add keys explicitly specified in _json_include list.
-        # That allows you to override those attributes unloaded above.
-        # For example, you may include some lazy-loaded relationship() there
-        # (which is usually removed at the step 1).
-        keys |= include
-
-        # 4. For objects in `deleted` or `detached` state, remove all
-        # relationships and lazy-loaded attributes, because they require
-        # refreshing data from the DB, but this cannot be done in these states.
-        # That is:
-        #  - if the object is deleted, you can't refresh data from the DB
-        #    because there is no data in the DB, everything is deleted
-        #  - if the object is detached, then there is no DB session associated
-        #    with the object, so you don't have a DB connection to send a query
-        # So in both cases you get an error if you try to read such attributes.
-        if ins.deleted or ins.detached:
-            keys -= relationships
-            keys -= unloaded
-
-        # 5. Delete all explicitly black-listed keys.
-        # That should be done last, since that may be used to hide some
-        # sensitive data from JSON representation.
-        keys -= exclude
-
-        return {key: getattr(self, key) for key in keys}
-
-    # noinspection PyMethodParameters
-    @declared_attr
-    def __tablename__(cls):
-        """Convert class name to snake_case table name"""
-        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', cls.__name__).lower()
-
-    @classmethod
-    def set_query(cls, query: SearchQuery) -> None:
-        cls._query = query
-
-    @property
-    def query(self) -> SearchQuery:
-        if self._query is not None:
-            return self._query
-        else:
-            raise NoSessionError("Can't get session. Please, call BaseModel.set_query() first")
-
-    # TODO: move get somewhere, maybe make a standalone query function
-    # @classmethod
-    # def get(cls, pk: str) -> Any:
-    #     """Get an instance by primary key"""
-    #     stmt = select(cls).where(cls.id == pk).limit(1)
-    #     session = db_session.get()
-    #     return session.execute(stmt).scalar_one()
-
-    id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-
-
-class BaseModel(_Base):
-    """
-    Separate BaseModel class to be able to include mixins and to Fix typing.
-
-    This should be used instead of Base.
-    """
-
-    metadata: ClassVar[MetaData]
-    query: ClassVar[SearchQuery]
-    # set_query: ClassVar[Callable[[SearchQuery], None]]
-
-    __abstract__ = True
-
-    __init__: Callable[..., _Base]  # type: ignore
-
-    def __repr__(self) -> str:
-        inst_state: InstanceState = sa_inspect(self)
-        attr_vals = [
-            f"{attr.key}={getattr(self, attr.key)}"
-            for attr in inst_state.mapper.column_attrs
-            if attr.key not in ["tsv"]
-        ]
-        return f"{self.__class__.__name__}({', '.join(attr_vals)})"
-
-
-class Reflected(DeferredReflection):
-    __abstract__ = True
-
-
-class ForeignKeyCascade(ForeignKey):
-    def __init__(self, *args, **kwargs):
-        kwargs['ondelete'] = "CASCADE"
-        super().__init__(*args, **kwargs)
 
 
 class WrappedSession(Session):
@@ -237,9 +79,6 @@ class Database:
         self.session_factory = sessionmaker(bind=self.engine, **SESSION_ARGUMENTS)
 
         self.scoped_session = scoped_session(self.session_factory, self._scopefunc)
-        query = cast(SearchQuery, self.scoped_session.query_property())
-        BaseModel.set_query(query)
-        Reflected.prepare(self.engine)
 
     def _scopefunc(self) -> Optional[str]:
         scope_str = self.request_context.get()
@@ -261,6 +100,7 @@ class Database:
         """
         token = self.request_context.set(str(uuid4()))
         self.scoped_session(**kwargs)
+
         yield self
         self.scoped_session.remove()
         self.request_context.reset(token)
--- server/db/models.py
+++ server/db/models.py
@@ -1,19 +1,142 @@
 from __future__ import annotations
+from __future__ import annotations
 
+import re
 from contextvars import ContextVar
-from typing import Any
+from typing import Any, List, Set, Dict, ClassVar, Callable, cast
+from uuid import uuid4
 
 from llama_index.constants import DEFAULT_EMBEDDING_DIM
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import String, UniqueConstraint
+from sqlalchemy import String, UniqueConstraint, inspect as sa_inspect, UUID, MetaData, ForeignKey
 from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import JSON
-from sqlalchemy.orm import Mapped
+from sqlalchemy.ext.declarative import as_declarative, DeferredReflection
+from sqlalchemy.orm import Mapped, declared_attr, InstanceState
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
 from db import db
-from db.database import BaseModel, ForeignKeyCascade, Reflected
+from db.database import SearchQuery
+
+
+@as_declarative()
+class _Base:
+    """SQLAlchemy base class."""
+
+    __abstract__ = True
+
+    _json_include: List = []
+    _json_exclude: List = []
+
+    def __json__(self, excluded_keys: Set = set()) -> Dict:  # noqa: B006
+        ins = sa_inspect(self)
+
+        columns = set(ins.mapper.column_attrs.keys())
+        relationships = set(ins.mapper.relationships.keys())
+        unloaded = ins.unloaded
+        expired = ins.expired_attributes
+        include = set(self._json_include)
+        exclude = set(self._json_exclude) | set(excluded_keys)
+
+        # This set of keys determines which fields will be present in
+        # the resulting JSON object.
+        # Here we initialize it with properties defined by the model class,
+        # and then add/delete some columns below in a tricky way.
+        keys = columns | relationships
+
+        # 1. Remove not yet loaded properties.
+        # Basically this is needed to serialize only .join()'ed relationships
+        # and omit all other lazy-loaded things.
+        if not ins.transient:
+            # If the entity is not transient -- exclude unloaded keys
+            # Transient entities won't load these anyway, so it's safe to
+            # include all columns and get defaults
+            keys -= unloaded
+
+        # 2. Re-load expired attributes.
+        # At the previous step (1) we substracted unloaded keys, and usually
+        # that includes all expired keys. Actually we don't want to remove the
+        # expired keys, we want to refresh them, so here we have to re-add them
+        # back. And they will be refreshed later, upon first read.
+        if ins.expired:
+            keys |= expired
+
+        # 3. Add keys explicitly specified in _json_include list.
+        # That allows you to override those attributes unloaded above.
+        # For example, you may include some lazy-loaded relationship() there
+        # (which is usually removed at the step 1).
+        keys |= include
+
+        # 4. For objects in `deleted` or `detached` state, remove all
+        # relationships and lazy-loaded attributes, because they require
+        # refreshing data from the DB, but this cannot be done in these states.
+        # That is:
+        #  - if the object is deleted, you can't refresh data from the DB
+        #    because there is no data in the DB, everything is deleted
+        #  - if the object is detached, then there is no DB session associated
+        #    with the object, so you don't have a DB connection to send a query
+        # So in both cases you get an error if you try to read such attributes.
+        if ins.deleted or ins.detached:
+            keys -= relationships
+            keys -= unloaded
+
+        # 5. Delete all explicitly black-listed keys.
+        # That should be done last, since that may be used to hide some
+        # sensitive data from JSON representation.
+        keys -= exclude
+
+        return {key: getattr(self, key) for key in keys}
+
+    # noinspection PyMethodParameters
+    @declared_attr
+    def __tablename__(cls):
+        """Convert class name to snake_case table name"""
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', cls.__name__).lower()
+
+    # TODO: move get somewhere, maybe make a standalone query function
+    # @classmethod
+    # def get(cls, pk: str) -> Any:
+    #     """Get an instance by primary key"""
+    #     stmt = select(cls).where(cls.id == pk).limit(1)
+    #     session = db_session.get()
+    #     return session.execute(stmt).scalar_one()
+
+    id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+
+
+class BaseModel(_Base):
+    """
+    Separate BaseModel class to be able to include mixins and to Fix typing.
+
+    This should be used instead of Base.
+    """
+
+    metadata: ClassVar[MetaData]
+    query: ClassVar[SearchQuery] = cast(SearchQuery, db.scoped_session.query_property())
+
+    __abstract__ = True
+
+    __init__: Callable[..., _Base]  # type: ignore
+
+    def __repr__(self) -> str:
+        inst_state: InstanceState = sa_inspect(self)
+        attr_vals = [
+            f"{attr.key}={getattr(self, attr.key)}"
+            for attr in inst_state.mapper.column_attrs
+            if attr.key not in ["tsv"]
+        ]
+        return f"{self.__class__.__name__}({', '.join(attr_vals)})"
+
+
+class Reflected(DeferredReflection):
+    __abstract__ = True
+
+
+class ForeignKeyCascade(ForeignKey):
+    def __init__(self, *args, **kwargs):
+        kwargs['ondelete'] = "CASCADE"
+        super().__init__(*args, **kwargs)
 
 
 class User(BaseModel, Reflected):
@@ -38,7 +161,7 @@ class Org(BaseModel):
             where(Chunk.org_id == self.id). \
             order_by(Chunk.embedding.cosine_distance(embedding)). \
             limit(k)
-        return list(map(tuple, db.session.get().execute(q).all()))
+        return list(map(tuple, db.session.execute(q).all()))
 
 
 class OrgUser(BaseModel):
@@ -57,3 +180,6 @@ class Chunk(BaseModel):
     data: Mapped[dict[str, Any]] = mapped_column(JSON)
     hash_value: Mapped[str] = mapped_column(String(64))
     embedding: Mapped[Vector] = mapped_column(Vector(DEFAULT_EMBEDDING_DIM))
+
+
+Reflected.prepare(db.engine)
