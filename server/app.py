696f0a07d63a230267923ef54b756473a9498c66
Serge Mosin <svmosin@gmail.com>
2024-01-02 18:53:53 +0100

    Switch to Blueprints

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from helpers import app
-from modules.module_1.routes import hello, hello_uid
+from chalice import Chalice
 
-# Hello routes
-app.route('/hello', methods=['GET'])(hello)
-app.route('/hello/{name}', methods=['GET'])(hello_uid)
+from modules.module_1.routes import bp as module_1_bp
+
+app = Chalice(app_name='blueprint-demo')
+app.register_blueprint(module_1_bp)
