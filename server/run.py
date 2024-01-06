aea5fef3b62ae8b925145c16182e449b72dbcea0
Serge Mosin <svmosin@gmail.com>
2024-01-06 11:10:47 +0100

    Extract main CLI runner

--- server/run.py
+++ server/run.py
@@ -21,7 +21,39 @@ logger = logging.getLogger(__name__)
 # TODO:
 # 1. Fix URL parsing list error
 # 2. Fix Wandb session serializing
-def main():
+def main(mode: str, org: Org, query: str, crawl_depth: int) -> None:
+    with db.session:
+        try:
+            org = db.session.execute(select(Org).where(Org.name == org)).scalar_one()
+        except exc.NoResultFound:
+            org = OrgFactory.create(name=org)
+        Org.current.set(org)
+
+        if mode == 'vdb':
+            if not query:
+                # no query provided, let's store the documents
+                archive_urls(app_settings.KNOWLEDGE_URLS.split(','), crawl_depth)
+            else:
+                results = retrieve(query)
+                pprint(results)
+        elif mode == "librarian":
+            while True:
+                user_input = input('>>> ')
+                response = librarian_agent().run(user_input)
+                print(response)
+        elif mode == "chat":
+            agent = get_agent()
+
+            set_verbose(True)
+            # set_debug(True)
+            while True:
+                user_input = input('>>> ')
+                response = agent.run(user_input)
+
+                print(response)
+
+
+if __name__ == "__main__":
     # Create the parser
     parser = argparse.ArgumentParser(description='Call foo function with org_id')
     # Add the arguments
@@ -35,36 +67,4 @@ def main():
                         default=0)
     args = parser.parse_args()
 
-    with db.session:
-        try:
-            org = db.session.execute(select(Org).where(Org.name == args.org)).scalar_one()
-        except exc.NoResultFound:
-            org = OrgFactory.create(name=args.org)
-        Org.current.set(org)
-
-        if args.mode == 'vdb':
-            if not args.query:
-                # no query provided, let's store the documents
-                archive_urls(app_settings.KNOWLEDGE_URLS.split(','), args.crawl_depth)
-            else:
-                results = retrieve(args.query)
-                pprint(results)
-        elif args.mode == "librarian":
-            while True:
-                user_input = input('>>> ')
-                response = librarian_agent().run(user_input)
-                print(response)
-        elif args.mode == "chat":
-            agent = get_agent()
-
-            set_verbose(True)
-            # set_debug(True)
-            while True:
-                user_input = input('>>> ')
-                response = agent.run(user_input)
-
-                print(response)
-
-
-if __name__ == "__main__":
-    main()
+    main(args.mode, args.org, args.query, args.crawl_depth)
