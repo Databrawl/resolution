c6dea836908762baf8bae0486342283ddd8fbdf1
Serge Mosin <svmosin@gmail.com>
2024-01-30 11:52:42 +0100

    Update local deployment script

--- deploy/run_local.sh
+++
@@ -1,4 +0,0 @@
-# make sure you have env.json file in this directory
-
-sam build --use-container
-sam local start-api --env-vars env.json --port 5050
--- scripts/be.sh
+++ scripts/be.sh
@@ -1,2 +1,5 @@
-cd ../deploy
-./run_local.sh
+cd ../deploy || exit
+
+sam build --use-container
+# make sure you have env.json file in deploy directory
+sam local start-api --env-vars env.json --port 5050
