fa7aaaf9a8f3b2ab032d2d929a6f82602b6a5e1f
Serge Mosin <svmosin@gmail.com>
2024-02-25 12:24:45 +0600

    Parametrize FE launch script

--- scripts/fe.sh
+++ scripts/fe.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+  echo "No environment specified, defaulting to 'prod'"
+  ENV="prod"
+else
+  ENV=$1
+fi
+
 set -a
-source ../.env.prod
+source ../.env.$ENV
 set +a
 cd ../client
 yarn dev
