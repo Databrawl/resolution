9a32c00a5c79804449aeb2c040c4b6caee13d31c
Serge Mosin <svmosin@gmail.com>
2024-03-11 12:44:06 +0100

    Refactor team

--- server/bots/team.py
+++ server/bots/team.py
@@ -40,7 +40,7 @@ def call_manager(memory: ConversationBufferWindowMemory) -> AgentExecutor:
             description="When the query of the customer is related to the product knowledge and any details about it.\nHow to interact: Provide what search keywords he should look for in the product knowledge base and give you back the relevant information he has retrieved.",
         ),
         Tool.from_function(
-            func=retrieval_chain_2(org_prompts).invoke,
+            func=feedback_chain(org_prompts).invoke,
             name="Feedback_assistant",
             description="When the query if the customer is related to the feedback, improvements or feature request.\nHow to interact: Provide the details of the customer's query and if this is not the first iteration of the feedback, make sure to provide a context that was already been discussed with the customer to not repeat the same things.",
         ),
@@ -49,18 +49,13 @@ def call_manager(memory: ConversationBufferWindowMemory) -> AgentExecutor:
             name="Issues_assistant",
             description="When the query of the customer is related to a bug, a problem or any difficulty that he is struggling with.\nHow to interact: Provide the information of what problem does the customer has with the context of previous advice and help that you provided to the customer and what customer has already tried to solve it. It is important for you both to not repeat same questions/advices to the customer. Also try not to ask more than 5 questions in one message.",
         ),
-        # Tool.from_function(
-        #    func=retrieval_chain_4().invoke,
-        #    name="Account_Management_assistant",
-        #    description="When the customer wants to make an account management operation.",
-        # ),
         Tool.from_function(
-            func=retrieval_chain_5(org_prompts).invoke,
+            func=switch_to_human_chain(org_prompts).invoke,
             name="Switch_to_human_assistant",
             description="When the customer wants to transfer the request to the human or you decide it yourself because this is the only solution.\nHow to interact: Provide the details about the status of the conversation and the level of satisfaction and mood level of the client at this moment.",
         ),
         Tool.from_function(
-            func=retrieval_chain_6(org_prompts).invoke,
+            func=fatality_chain(org_prompts).invoke,
             name="Conversation_finisher",
             description="When you need to manage all types of conversation endings with the customer.\nHow to interact with this team member: Tell that the customer is ready to finish the conversation and provide his satisfaction level, mood level and the current conversation status.",
         )
@@ -96,6 +91,7 @@ def retrieval_chain_1(prompts: dict[str, str]) -> RunnableSerializable[str, str]
                      model_name=app_settings.GPT_35)
     return (
             {
+                "user_question": RunnablePassthrough(),
                 "query": RunnablePassthrough(),
                 "context": RunnablePassthrough() | retriever | format_docs,
             }
@@ -105,7 +101,7 @@ def retrieval_chain_1(prompts: dict[str, str]) -> RunnableSerializable[str, str]
     )
 
 
-def retrieval_chain_2(prompts: dict[str, str]) -> RunnableSerializable[str, str]:
+def feedback_chain(prompts: dict[str, str]) -> RunnableSerializable[str, str]:
     """
     Feedback assistant. Deals with any feedback type queries from the customer.
     """
@@ -160,49 +156,7 @@ def get_agent_issuer(prompts: dict[str, str]) -> AgentExecutor:
     )
 
 
-# def retrieval_chain_3() -> RunnableSerializable[str, str]:
-#     """
-#     Issuer. Deals with any types of customers' problems.
-#     """
-
-#     prompt = PromptTemplate.from_template(app_settings.PROMPTS['issuer'])
-#     retriever = LlamaVectorIndexRetriever(metadata={"current_org": Org.current.get()})
-
-#     llm = ChatOpenAI(temperature=0,
-#                      openai_api_key=app_settings.OPENAI_API_KEY,
-#                      model_name=app_settings.GPT_35)
-#     return (
-#             {
-#                 "query": RunnablePassthrough(),
-#                 "context": RunnablePassthrough() | retriever | format_docs,
-#             }
-#             | prompt
-#             | llm
-#             | StrOutputParser()
-#     )
-
-# def retrieval_chain_4() -> RunnableSerializable[str, str]:
-#     """
-#     Account Management assistant. Deals with any account related stuff.
-#     """
-
-#     prompt = PromptTemplate.from_template(app_settings.PROMPTS['account'])
-#     retriever = LlamaVectorIndexRetriever(metadata={"current_org": Org.current.get()})
-
-#     llm = ChatOpenAI(temperature=0,
-#                      openai_api_key=app_settings.OPENAI_API_KEY,
-#                      model_name=app_settings.GPT_35)
-#     return (
-#             {
-#                 "query": RunnablePassthrough(),
-#                 "context": RunnablePassthrough() | retriever | format_docs,
-#             }
-#             | prompt
-#             | llm
-#             | StrOutputParser()
-#     )
-
-def retrieval_chain_5(prompts: dict[str, str]) -> RunnableSerializable[str, str]:
+def switch_to_human_chain(prompts: dict[str, str]) -> RunnableSerializable[str, str]:
     """
     Switch to human. Allows transfer requests and support to the internal team.
     """
@@ -222,7 +176,7 @@ def retrieval_chain_5(prompts: dict[str, str]) -> RunnableSerializable[str, str]
     )
 
 
-def retrieval_chain_6(prompts: dict[str, str]) -> RunnableSerializable[str, str]:
+def fatality_chain(prompts: dict[str, str]) -> RunnableSerializable[str, str]:
     """
     Conversation finisher. Knows how to make a nice end to the conversation.
     """
