f6c98181526b7b332f0a88fffc7526b36b1f9ec9
Serge Mosin <svmosin@gmail.com>
2024-03-23 13:28:42 +0100

    Add Flask-SQLAlchemy dependency

--- client/lib/context/ChatProvider/mocks/ChatProviderMock.tsx
+++ client/lib/context/ChatProvider/mocks/ChatProviderMock.tsx
@@ -14,7 +14,7 @@ export const ChatProviderMock = ({
             value={{
                 messages: [],
                 setMessages: () => void 0,
-                updateStreamingHistory: () => void 0,
+                updateChatHistory: () => void 0,
                 notifications: [],
                 setNotifications: () => void 0,
                 removeMessage: () => void 0,
