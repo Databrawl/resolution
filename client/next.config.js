9e67ecc4bca64009bedf15ccbc02ad7b4926bf9c
Serge Mosin <svmosin@gmail.com>
2024-03-23 13:42:59 +0100

    Ignore ESLint errors on build

--- client/next.config.js
+++ client/next.config.js
@@ -28,6 +28,11 @@ const nextConfig = {
             },
         ];
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 const ContentSecurityPolicy = {
