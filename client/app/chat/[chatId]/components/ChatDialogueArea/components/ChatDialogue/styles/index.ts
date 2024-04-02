89b0ef8b3d3f797e86db4cd99d5728a2227a1fd9
Serge Mosin <svmosin@gmail.com>
2024-04-02 09:39:26 +0100

    Reduce margin between chat and quick actions
    https://www.notion.so/databrawl/Decrease-the-quick-actions-block-size-c8d7259f44c74263b9f73fdf94880acb?pvs=4

--- client/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/styles/index.ts
+++ client/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/styles/index.ts
@@ -1,4 +1,4 @@
 export const chatItemContainerClassName = "flex flex-col gap-3";
 
 export const chatDialogueContainerClassName =
-    "flex flex-col flex-1 overflow-y-auto mb-10";
+    "flex flex-col flex-1 overflow-y-auto mb-1";
