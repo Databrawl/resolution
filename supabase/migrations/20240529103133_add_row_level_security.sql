a778a33cd3b6921760aabb0469f8a526b527ab8f
Serge Mosin <svmosin@gmail.com>
2024-06-02 11:52:31 +0100

    Add row-level security to all tables

---
+++ supabase/migrations/20240529103133_add_row_level_security.sql
@@ -0,0 +1,17 @@
+alter table "public"."alembic_version" enable row level security;
+
+alter table "public"."chat" enable row level security;
+
+alter table "public"."chunk" enable row level security;
+
+alter table "public"."message" enable row level security;
+
+alter table "public"."onboarding" enable row level security;
+
+alter table "public"."org" enable row level security;
+
+alter table "public"."org_user" enable row level security;
+
+alter table "public"."test_tenant" enable row level security;
+
+
